1c17eb253d43f39dcf87717fc7807c30a12302c1
Erick Orlandini <112948249+erickorlandini@users.noreply.github.com>
2025-04-28 14:55:16 -0300

    Update globalContext.ts
    Mudança no acesso

--- tests/support/data/globalContext.ts
+++ tests/support/data/globalContext.ts
@@ -2,8 +2,8 @@
 export  const context = {
     gerais: {
         usuario: {
-            email: "76@m.com",
-            senha: "123456"
+            email: "",
+            senha: ""
         }
     }
 }
